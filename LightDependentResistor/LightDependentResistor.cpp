39350b121d4048aea87e8384a2f17fb3941d6f90
Quentin Comte-Gaz <quentin@comte-gaz.com>
2018-03-26 21:53:31 +0200

    Fix #2 (part 2)

--- LightDependentResistor/LightDependentResistor.cpp
+++ LightDependentResistor/LightDependentResistor.cpp
@@ -60,12 +60,12 @@ void LightDependentResistor::updatePhotocellParameters(float mult_value, float p
 
 float LightDependentResistor::luxToFootCandles(float intensity_in_lux)
 {
-  return intensity_in_footcandles/10.764;
+  return intensity_in_lux/10.764;
 }
 
 float LightDependentResistor::footCandlesToLux(float intensity_in_footcandles)
 {
-  return 10.764*intensity_in_lux;
+  return 10.764*intensity_in_footcandles;
 }
 
 void LightDependentResistor::setPhotocellPositionOnGround(bool on_ground)
